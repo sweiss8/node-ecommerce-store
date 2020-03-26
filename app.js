92eca2fc730d61600f9215805fd57ef13e4ec78d
sweiss8 <stuart@Stuarts-MacBook-Pro.local>
2020-03-26 16:42:58 -0600

    added admin controllers and routes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,9 +34,11 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://sweiss8:lant!rhih1FRA@cluster0-znwg3.mongodb.net/shop?retryWrites=true&w=majority'
+    'mongodb+srv://sweiss8:lant!rhih1FRA@cluster0-znwg3.mongodb.net/shop?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true}
   )
   .then(result => {
+    console.log('Connected to MongoDB!')
     app.listen(3000);
   })
   .catch(err => {
--- controllers/admin.js
+++ controllers/admin.js
@@ -8,21 +8,20 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
+// If this doesn't work, add object keys in Product
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(
-    title,
+  const product = new Product({
     price,
-    description,
+    title,
     imageUrl,
-    null,
-    req.user._id
-  );
+    description
+  });
   product
-    .save()
+    .save() //this method is provided by Mongoose
     .then(result => {
       // console.log(result);
       console.log('Created Product');
@@ -62,24 +61,23 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  );
-  product
-    .save()
+
+  Product.findById(prodId).then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save()
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
+  })
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -92,7 +90,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
